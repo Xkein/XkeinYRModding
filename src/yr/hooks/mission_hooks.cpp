664e6924ea74fdb8b41cf5db63252c9e73b92bae
Xkein <739502808@qq.com>
2025-05-28 23:55:10 +0800

    [core] fix YrMissionExecuteEvent wrong override address

--- src/yr/hooks/mission_hooks.cpp
+++ src/yr/hooks/mission_hooks.cpp
@@ -2,7 +2,6 @@
 #include "yr/yr_hook.h"
 #include <FootClass.h>
 
-IMPL_HOOK_OVERRIDE_RETURN_ADDRESS(YrMissionExecuteEvent, 0x5B30B2, 0x5B30D8)
 BROADCAST_HOOK_EVENT(0x5B30B2, 0x6, YrMissionExecuteEvent)
 {
     E->pMission = R->EDI<MissionClass*>();
@@ -11,6 +10,7 @@ IMPL_HOOK_BROADCAST(YrMissionExecuteEvent, 0x5B30B2) {
     auto retAddr = hookEvent->Broadcast(R, E);
     if (E->hasSet) {
         E->pMission->UpdateTimer.Start(E->returnValue);
+        return 0x5B34E0;
     }
     return retAddr;
 }
